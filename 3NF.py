d2502f3047fcda832918828f79b4e71df9e30e83
Mateo Mamut <vladom156@gmail.com>
2019-06-06 02:56:41 +0200

    Razdvojeno (donekle) u funkcije, poboljsana (nadam se) imena i napravljeno da ako je u 3nf da se ne normalizira

--- 3NF.py
+++ 3NF.py
@@ -1,88 +1,90 @@
 import itertools
-def convertTuple(tup): 
-    str =  ''.join(tup) 
+def convertTuple(tup):
+    str =  ''.join(tup)
     return str
 
+def checkForm(R, Fmin, K):
+    for fo in Fmin:
+        isValid = True
+        leftSide, rightSide = fo.split('->')
+        #Prvi uvjet
+        for att in rightSide:
+            if att not in leftSide:
+                isValid = False
+
+        if isValid:
+            continue
+
+        #Drugi uvjet
+        for temp_k in K:
+            isValid = True
+            for att in temp_k:
+                if att not in leftSide:
+                    isValid = False
+
+            if isValid:
+                break
+
+        if isValid:
+            continue
+
+        #Treci uvjet
+        for temp_k in K:
+            isValid = True
+            for att in rightSide:
+                if att not in temp_k:
+                    #Zadnji uvjet pa ako on nije ispunjen mozemo odmah vratit False
+                    isValid = False
+
+            if isValid:
+                break
+
+        if not isValid:
+            return False
+
+    return True
+
+def normalize(R, Fmin):
+    noviR=[]
+    for fo in Fmin:
+        fo = ''.join(set(fo.split('->')))
+        isIncluded = False
+        for temp_R in noviR:
+            isIncluded = True
+            for att in fo:
+                if att not in temp_R:
+                    isIncluded = False
+
+            if isIncluded:
+                break
+
+        if not isIncluded:
+            noviR.append(fo)
+
+    #Dodaj kljuc ako ga vec nema
+    keyIncluded = False
+    for key in K:
+        key = ''.join(sorted(key))
+        for fo in Fmin:
+            fo = ''.join(sorted(set(fo.split('->'))))
+            if key in fo:
+                keyIncluded = True
+                break
+
+        if keyIncluded:
+            print('Kljuc je vec ukljucen, a on je: ' + key)
+            break
+            
+    if not keyIncluded:
+        noviR.append(K[0])
+
+    return noviR
 
 R = "ABCDEF"
 Fmin = ["A->B", "A->CD", "A->C", "C->BF", "E->BD"]
 K=["A", "B", "C", "E"]
-canonical_form = []
-for att in Fmin:
-    x = att.split('->')
-    if(len(x[1])>1):
-        for z in x[1]:
-            canonical_form.append(x[0] + "->" + z)
-    else:
-        canonical_form.append(att)
 
-print(Fmin)
-print(canonical_form)
-
-jel_treba_normlizirati = False
-for fo in Fmin:
-    dobar = True
-    x,y=fo.split('->')
-    #Prvi uvjet
-    for att in y:
-        if att not in x:
-            dobar = False
-    
-    #Drugi uvjet
-    if not dobar:
-        for temp_k in K:
-            dobar = True
-            for att in temp_k:
-                if att not in x:
-                    dobar = False
-            if dobar:
-                break
-            
-        #Treci uvjet
-        if not dobar:
-            for temp_k in K:
-                dobar = True
-                for att in y:
-                    if att not in temp_k:
-                        dobar = False
-                if dobar:
-                    break
-    #Provjera jel oke
-    if not dobar:
-        jel_treba_normlizirati = True
-
-if not jel_treba_normlizirati:
+if checkForm(R, Fmin, K):
     print("Vec je u 3. NF")
-
-noviR=[]
-
-for fo in Fmin:
-    fo = ''.join(set(fo.split('->')))
-    isIncluded = False
-    for temp_R in noviR:
-        isIncluded = True
-        for att in fo:
-            if att not in temp_R:
-                isIncluded = False
-        if isIncluded:
-            break
-    
-    if not isIncluded:
-        noviR.append(fo)
-
-#Dodaj kljuc ako ga vec nema
-sadrziKljuc = False
-for kljuc in K:
-    kljuc = ''.join(sorted(kljuc))
-    for fo in Fmin:
-        fo = ''.join(sorted(set(fo.split('->'))))
-        if kljuc in fo:
-            sadrziKljuc = True
-            break
-    if sadrziKljuc:
-        print('Kljuc je vec ukljucen, a on je: ' + kljuc)
-        break
-if not sadrziKljuc:
-    noviR.append(K[0])
-    
-print(noviR)
+else:
+    print(normalize(R, Fmin))
